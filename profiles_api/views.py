80716965d41683ea94768dc1a54d37c4d0284d41
Jihyun-Choi <70438098+Jihyun-Choi@users.noreply.github.com>
2022-01-20 01:02:26 +0900

    Add POST method to APIView - 34

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,13 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializers_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
-
         an_apiview = [
             'Uses HTTP methods as functions (get, post, patch, put, delete)',
             'Is similar to a traditional Django View',
@@ -16,3 +19,18 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    #POST 함수 정의
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)       #API view에 대한 요청은 request.data로 전달
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message' : message})
+        else:
+            return Response(    #기본적으로 Response는 HTTP 200을 반환
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST  #API에 의한 잘못된 request 오류
+            )
